cc9d55cff9fd6131bfa449f790b6f06ec10eb5f8
realPiyushX <jee49553@gmail.com>
2026-02-26 13:11:32 +0530

    Refactor service worker for caching and activation
    Updated service worker to use a new cache name and added activation event handling to delete old caches.

--- sw.js
+++ sw.js
@@ -1,14 +1,34 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('jee-store').then((cache) => cache.addAll([
-      '/index.html',
-    ])),
+const CACHE_NAME = "phypy-v1";
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html"
+      ]);
+    })
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  console.log(e.request.url);
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request)),
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
